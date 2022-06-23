8823c8e377225483c8c14112a69318e71cba4c91
João Victor Lopes <91668424+ProjectzJV@users.noreply.github.com>
2022-06-23 12:34:31 -0300

    Update Ecommerce.sql

--- Ecommerce.sql
+++ Ecommerce.sql
@@ -15,8 +15,8 @@ CREATE TABLE tb_produtos(
 INSERT INTO tb_produtos(Produto, Valor, Categoria, QEstoque)VALUES("Xbox One X", 4800.00, "Games", 67);
 
 SELECT * FROM tb_produtos;
-SELECT * FROM tb_produtos WHERE Valor > 2000;
-SELECT * FROM tb_produtos WHERE Valor < 2000;
+SELECT * FROM tb_produtos WHERE Valor > 500;
+SELECT * FROM tb_produtos WHERE Valor < 500;
 
 
     
